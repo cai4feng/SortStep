489e2a26f25aed68420fde4b596e6e92a8e85818
cai4feng <cai4feng@gmail.com>
2015-04-07 10:09:00 -0700

    Update Stopwatch.java

--- Stopwatch.java
+++ Stopwatch.java
@@ -1,27 +1,26 @@
-
 public class Stopwatch {
   private long start;
   private long stop;
   public void start()
     /* begins keeping track of time */
   {
-    //your code here
+    start = System.nanoTime();
   }
   public void stop()
     /* stops keeping track of time */
   {
-    //your code here
+    stop = System.nanoTime();
   }
   public long elapsedTime()
     /* uses System.out to display the elapsed time in seconds */
   {
-    // Replace the following line with your code
-    return 0;
+    return (stop-start);
   }
   public void reset()
     /* resets the elapsed time to 0 */
   {
-    //your code here
+    long start = 0;
+    long stop = 0;
   }
 }
 
